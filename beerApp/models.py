a8851fa7b33cb1e911a9cbd9db5ff743c8d54805
Mike Kreiser <mikepkreiser@gmail.com>
2018-04-30 18:55:34 -0500

    Setup basic beer server

---
+++ beerApp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+# Create your models here. # Don't tell me what to do.
+
+class Beer(models.Model):
+  ranking = models.IntegerField()
+  brewery = models.CharField(max_length=50)
+  name = models.CharField(max_length=50)
+  type = models.CharField(max_length=50)
+  subtype = models.CharField(max_length=50)
+  city = models.CharField(max_length=50)
+  state = models.CharField(max_length=50)
+  country = models.CharField(max_length=50)
+  comments = models.TextField(blank=True)
+  # tags = ArrayField(models.CharField(max_length=50)) # Im being lazy
+
+  def __str__(self):
+    return self.brewery + ' ' + self.name
---
+++ beerApp/views.py
@@ -0,0 +1,49 @@
+from django.contrib.admin.views.decorators import staff_member_required
+
+from rest_framework import filters, generics
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.response import Response
+
+from beerApp.models import Beer
+from beerApp.serializers import BeerSerializer
+
+# Root page directory
+@api_view(('GET',))
+@permission_classes((AllowAny,))
+def api_root(request, format=None):
+    return Response({ "routes": [
+      "GET ALL Beers: https://kreiserbeerserver.herokuapp.com/beers/",
+      "GET Beer: https://kreiserbeerserver.herokuapp.com/beers/pourBeer/[id]/",
+      "POST Beer: https://kreiserbeerserver.herokuapp.com/beers/brewBeer/",
+      "PUT Beer: https://kreiserbeerserver.herokuapp.com/beers/mixBeer/[id]/",
+      "DELETE Beer: https://kreiserbeerserver.herokuapp.com/beers/tossBeer/[id]/",
+    ]})
+
+# MEET SERIALIZERS
+class BeerGETAll(generics.ListAPIView):
+  permission_classes = (AllowAny, )
+  queryset = Beer.objects.all()
+  serializer_class = BeerSerializer
+  filter_backends = (filters.SearchFilter,)
+  search_fields = ['name']
+
+class BeerGET(generics.RetrieveAPIView):
+  permission_classes = (AllowAny, )
+  queryset = Beer.objects.all()
+  serializer_class = BeerSerializer
+
+class BeerPOST(generics.CreateAPIView):
+  permission_classes = (IsAdminUser, )
+  queryset = Beer.objects.all()
+  serializer_class = BeerSerializer
+
+class BeerPUT(generics.RetrieveUpdateAPIView):
+  permission_classes = (IsAdminUser, )
+  queryset = Beer.objects.all()
+  serializer_class = BeerSerializer
+
+class BeerDELETE(generics.DestroyAPIView):
+  permission_classes = (IsAdminUser, )
+  queryset = Beer.objects.all()
+  serializer_class = BeerSerializer
