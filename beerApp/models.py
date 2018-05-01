8a38af2ca15d1b75148d508e95713b536f5b4c3d
Mike Kreiser <mikepkreiser@gmail.com>
2018-04-30 20:33:26 -0500

    Remove subtype

--- beerApp/models.py
+++ beerApp/models.py
@@ -8,7 +8,6 @@ class Beer(models.Model):
   brewery = models.CharField(max_length=50)
   name = models.CharField(max_length=50)
   type = models.CharField(max_length=50)
-  subtype = models.CharField(max_length=50)
   city = models.CharField(max_length=50)
   state = models.CharField(max_length=50)
   country = models.CharField(max_length=50)
